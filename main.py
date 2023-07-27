e676e6ab127c27a2061403807e771b91e85d4f94
Saksham Jain <iamsaksham81@gmail.com>
2023-07-27 11:19:49 +0530

    BC

---
+++ main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import streamlit as st
+from streamlit_lottie import st_lottie
+
+# with st.echo():
+_,c1,_ = st.columns((0.5,0.8,0.5))
+with c1:
+    st_lottie("animation_lkk1li9j.json")
+
+st.write('Hello, *World!* :sunglasses:')
